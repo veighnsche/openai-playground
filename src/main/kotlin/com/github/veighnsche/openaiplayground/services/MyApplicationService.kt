7318c698ea022aed7985f5bda776f64da5d4ae30
GitHub Action <action@github.com>
2022-07-26 07:44:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/veighnsche/openaiplayground/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.veighnsche.openaiplayground.services
+
+import com.github.veighnsche.openaiplayground.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
